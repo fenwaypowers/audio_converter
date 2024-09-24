7225ac6e48ca13d03fa52e4a6448653fba687e00
Fenway Powers <31604245+fenwaypowers@users.noreply.github.com>
2024-09-24 13:46:17 -0400

    2024 update: argparse, tempfile, black formatting

--- main.py
+++ main.py
@@ -1,134 +1,182 @@
-import sys, os, subprocess, audioread
+import os
+import subprocess
+import audioread
+import argparse
+import tempfile
+import shutil
 
-bitrate = 320 #(Kbps) don't need to modify if you are using target file size mode
-out_ext = "mp3" #output file you want (decides the encoder you use as well)
-target_size = 8 #in MB
+# Define constants
+DEFAULT_BITRATE = 320  # in Kbps
+DEFAULT_OUT_EXT = "mp3"
+DEFAULT_TARGET_SIZE = 8  # in MB
 
-encoders = {
-    "mp3":"libmp3lame",
-    "opus":"libopus",
-    "ogg":"libopus",
-    "aac":"aac",
-    "m4a":"aac",
-    "ac3":"ac3",
-    "mp2":"mp2",
-    "flac":"flac",
-    "wav":"wavpack",
-    "alac":"alac",
-    }
+# Supported encoders based on file extension
+ENCODERS = {
+    "mp3": "libmp3lame",
+    "opus": "libopus",
+    "ogg": "libopus",
+    "aac": "aac",
+    "m4a": "aac",
+    "ac3": "ac3",
+    "mp2": "mp2",
+    "flac": "flac",
+    "wav": "wavpack",
+    "alac": "alac",
+}
 
-directory = ""
-outdirectory = ""
-image_dir = "images"
-extensions = [".flac",".mp3",".aac",".ogg",".wav",".m4a",".alac",".opus",".ac3",".mp2", ".mka"]
-target_mode = False
+EXTENSIONS = [
+    ".flac",
+    ".mp3",
+    ".aac",
+    ".ogg",
+    ".wav",
+    ".m4a",
+    ".alac",
+    ".opus",
+    ".ac3",
+    ".mp2",
+    ".mka",
+]
 
-if (os.path.isdir(image_dir)) == False:
-    os.mkdir(image_dir)
 
-if len(sys.argv) > 1:
-    for i in range(1,len(sys.argv)):
-        if sys.argv[i] == "-help" or sys.argv[i] == '-h':
-            print('''
-Welcome to Audio Converter by Fenway Powers
-This program lets you convert entire folders of audio files with ease.
+# Parse arguments using argparse
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Audio Converter by Fenway Powers")
 
-Usage:
-python3 main.py -i [directory] -br [bitrate] -o [file extension] -target [size in MB]
-            
-Use -i to specify the directory you want to convert. If left blank, it will convert all the audio files in the program's directory.
+    parser.add_argument(
+        "-i",
+        "--input_dir",
+        type=str,
+        help="Input directory containing audio files",
+        default="",
+    )
+    parser.add_argument(
+        "-br",
+        "--bitrate",
+        type=int,
+        help="Bitrate for audio in kbps",
+        default=DEFAULT_BITRATE,
+    )
+    parser.add_argument(
+        "-o",
+        "--output_ext",
+        type=str,
+        help="Output file extension",
+        choices=ENCODERS.keys(),
+        default=DEFAULT_OUT_EXT,
+    )
+    parser.add_argument(
+        "-t", "--target", type=int, help="Target file size in MB", default=None
+    )
+    parser.add_argument("--codec", type=str, help="Custom codec to use for conversion")
 
-Use -br to specify the bitrate of your audio in kbps. If left blank, default is 320kbps.
+    return parser.parse_args()
 
-Use -o to specify the extension you want to output to. If left blank, MP3 is default.
 
-Use -target if you want to convert all your files to a certain size (in MB).
+# Get output directory based on input directory and output extension
+def get_output_directory(input_dir, output_ext):
+    if input_dir:
+        return os.path.join(input_dir, output_ext)
+    return output_ext
 
-Use -codec to specify the codec you would like to use. Only necessary if you are not going to use the default codecs for each extension.
 
-Use -default_codec to view the default codecs for each extension.''')
-            sys.exit(0)
-        elif sys.argv[i] == "-default_codec" or sys.argv[i] == '-default':
-            for enc in encoders:
-                print(enc + ": " + encoders[enc])
-            sys.exit(0)
-        elif sys.argv[i] == "-br":
-            if sys.argv[i+1].isdecimal and sys.argv[i+1]:
-                bitrate = int(sys.argv[i+1])
-            else:
-                print("Error: Incorrect use of -br, proper use is: -br [bitrate]\nExample: -br 320")
-                sys.exit(0)
+# Calculate bitrate for target file size mode
+def calculate_bitrate_for_target(file, target_size):
+    with audioread.audio_open(file) as f:
+        total_sec = f.duration
+    return int((target_size * 8192) / total_sec)  # bitrate calculation
 
-            increment=True
-        elif sys.argv[i] == "-o":
-            cont = False
-            if "."+sys.argv[i+1] in extensions:
-                cont = True
-                out_ext = sys.argv[i+1]
-            
-            if cont == False:
-                print("Error: incorrect use of -o. Correct use is: -o [extension]\nExample: -o mp3")
-                sys.exit(0)
 
-        elif sys.argv[i] == "-codec":
-            cont = False
-            if "-" not in sys.argv[i+1]:
-                for ext in encoders:
-                    encoders[ext] = sys.argv[i+1]
-                cont = True
+# Process each file for conversion
+def process_files(
+    input_dir, output_ext, bitrate, target_mode, target_size, codec, temp_dir
+):
+    if input_dir:
+        files = os.listdir(input_dir)
+    else:
+        files = os.listdir()
 
-        elif sys.argv[i] == "-i":
-            if os.path.exists(sys.argv[i+1]):
-                if sys.argv[i+1][len(sys.argv[i+1])-1] != "/":
-                    directory = sys.argv[i+1]+"/"
+    output_dir = get_output_directory(input_dir, output_ext)
+
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+
+    for file in files:
+        for ext in EXTENSIONS:
+            if ext in file:
+                if target_mode:
+                    bitrate = calculate_bitrate_for_target(file, target_size)
+
+                cut = len(ext)
+                img_path = os.path.join(temp_dir, file[:-cut] + ".jpg")
+                output_file = os.path.join(output_dir, file[:-cut] + "." + output_ext)
+
+                # Extract album art
+                subprocess.run(
+                    [
+                        "ffmpeg",
+                        "-y",
+                        "-i",
+                        os.path.join(input_dir, file),
+                        "-an",
+                        img_path,
+                    ]
+                )
+
+                # Convert audio file
+                command = [
+                    "ffmpeg",
+                    "-y",
+                    "-i",
+                    os.path.join(input_dir, file),
+                    "-b:a",
+                    f"{bitrate}k",
+                    "-map_metadata",
+                    "0",
+                    "-map_metadata",
+                    "0:s:0",
+                    "-id3v2_version",
+                    "3",
+                    "-vn",
+                    output_file,
+                ]
+
+                if codec:
+                    command.insert(6, "-c:a")
+                    command.insert(7, codec)
                 else:
-                    directory = sys.argv[i+1]
-            else:
-                print("Error: incorrect use of -i. Correct use is: -i [path]\nMake sure your input was a real directory.\nExample: -i \'music/album\'")
-                sys.exit(0)
+                    command.insert(6, "-c:a")
+                    command.insert(7, ENCODERS[output_ext])
 
-        elif sys.argv[i] == "-target":
-            if sys.argv[i+1].isdecimal:
-                target_size = int(sys.argv[i+1])
-                target_mode = True
-            else:
-                print("Error: incorrect use of -target. Correct use is: -target [size]\nExample: -target 6")
-                sys.exit(0)
+                subprocess.run(command)
 
-if outdirectory == "":
-    outdirectory = directory+out_ext
+                # Embed album art if output is mp3
+                if output_ext == "mp3" and os.path.isfile(img_path):
+                    subprocess.run(
+                        ["eyeD3", "--add-image", img_path + ":FRONT_COVER", output_file]
+                    )
 
-if directory != "":
-    dir_list = os.listdir(directory)
-else:
-    dir_list = os.listdir()
 
-if (os.path.isdir(outdirectory)) == False:
-    os.mkdir(outdirectory)
+# Main function
+def main():
+    args = parse_arguments()
 
-for file in dir_list:
-    for ext in extensions:
-        if ext in file:
-            
-            if target_mode:
-                with audioread.audio_open(file) as f:
-                        totalsec = f.duration
-                bitrate = int((target_size*8192)/(totalsec)) #math for calculating bitrate for target file size (in MB)
+    input_dir = args.input_dir
+    bitrate = args.bitrate
+    output_ext = args.output_ext
+    target_size = args.target
+    codec = args.codec
+    target_mode = target_size is not None
 
-            cut = len(ext)
+    # Create a temporary directory for storing images
+    with tempfile.TemporaryDirectory() as temp_dir:
+        print(f"Using temporary directory: {temp_dir}")
 
-            img_dir = image_dir+"/"+file[0:-(cut)]+".jpg"
+        # Process files for conversion
+        process_files(
+            input_dir, output_ext, bitrate, target_mode, target_size, codec, temp_dir
+        )
 
-            subprocess.run(["ffmpeg","-y","-i",directory+file,"-an",img_dir])
-            
-            command = ["ffmpeg","-y","-i",directory+file,"-b:a",str(bitrate)+"k","-map_metadata","0","-map_metadata","0:s:0","-id3v2_version","3","-vn",outdirectory+"/"+file[0:-(cut)]+"."+out_ext]
 
-            subprocess.run(command)
-            
-            if out_ext == "mp3" and (os.path.isfile(img_dir)):
-                subprocess.run(["eyeD3","--add-image",img_dir+":FRONT_COVER",outdirectory+"/"+file[0:-(cut)]+"."+out_ext])
-
-for img in os.listdir(image_dir):
-    os.remove(image_dir+"/"+img)
-
-os.rmdir(image_dir)
+if __name__ == "__main__":
+    main()
